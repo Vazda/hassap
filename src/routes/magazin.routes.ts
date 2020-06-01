ede228aaa8ea4e9fa50ef474737c48d834ac5146
Kerim Balic <balickerim@gmail.com>
2020-06-01 10:49:45 +0200

    lint

--- src/routes/magazin.routes.ts
+++ src/routes/magazin.routes.ts
@@ -1,19 +1,19 @@
-import { Router } from "express";
-import passport from "passport";
-import magazin from "../controllers/magazin.controller";
+import { Router } from 'express';
+import passport from 'passport';
+import magazin from '../controllers/magazin.controller';
 
 const router = Router();
 
 router
-  .route("/")
+  .route('/')
   .get(magazin.getMagazine)
   .post(
-    passport.authenticate("jwt", { session: false }),
-    magazin.addNewMagazin
+    passport.authenticate('jwt', { session: false }),
+    magazin.addNewMagazin,
   );
 
 router
-  .route("/:magazinId")
+  .route('/:magazinId')
   .get(magazin.getMagazineById)
   .put(magazin.updateMagazin)
   .delete(magazin.deleteMagazin);

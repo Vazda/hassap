b887c83738eeeb70cd5e8c2cd795b385596f70da
Nermin Delic <vazda18@gmail.com>
2020-06-04 13:58:25 +0200

    added authentication on new sponsors route

--- src/routes/sponsor.routes.ts
+++ src/routes/sponsor.routes.ts
@@ -1,21 +1,26 @@
-import { Router } from 'express';
-import passport from 'passport';
-import sponsors from '../controllers/sponsors.controller';
+import { Router } from "express";
+import passport from "passport";
+import sponsors from "../controllers/sponsors.controller";
 
 const router = Router();
 
 router
-  .route('/')
+  .route("/")
   .get(sponsors.getSponsors)
   .post(
-    passport.authenticate('jwt', { session: false }),
-    sponsors.addNewSponsor,
+    passport.authenticate("jwt", { session: false }),
+    sponsors.addNewSponsor
   );
 
-router.route('/business/:companyId').get(sponsors.getBusinessCardAvailability);
+router
+  .route("/business/:companyId")
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    sponsors.getBusinessCardAvailability
+  );
 
 router
-  .route('/:sponsorId')
+  .route("/:sponsorId")
   .get(sponsors.getSponsorById)
   .put(sponsors.updateSponsor)
   .delete(sponsors.deleteSponsor);

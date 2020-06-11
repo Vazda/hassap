dd51eb271facc39faf15b8422fcfe3e038221fb2
Edis Banjic <banjicedis@gmail.com>
2020-06-11 15:09:33 +0200

    feat: add business endpoint

--- src/controllers/sponsormngmnt.controller.ts
+++ src/controllers/sponsormngmnt.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import _ from "lodash";
 import { generateError } from "../adapters/response";
 import SponsorMngmnt from "../models/sponsormngmnt.model";
+import Sponsor from "../models/sponsor.model";
 
 const getSponsors = async (req: Request, res: Response) => {
   try {
@@ -16,6 +17,7 @@ const getSponsors = async (req: Request, res: Response) => {
 
 const getBusinessCardAvailability = async (req: Request, res: Response) => {
   try {
+    console.log("kjskjskj");
     const isBusinessCardUserExists = await SponsorMngmnt.exists({
       businessCardUser: true,
       company: req.params.companyId
@@ -27,6 +29,24 @@ const getBusinessCardAvailability = async (req: Request, res: Response) => {
       .send({ msg: generateError("Error fetching Sponsors"), error: e });
   }
 };
+
+const getBusinessSponsors = async (req: Request, res: Response) => {
+  try {
+    const { sponsorId } = req.params;
+    const sponsor = await Sponsor.findOne({ _id: sponsorId });
+    const bussinessPerson = await SponsorMngmnt.findOne({
+      company: sponsor.name,
+      businessCardUser: true
+    });
+
+    return res.send(bussinessPerson);
+  } catch (e) {
+    return res
+      .status(404)
+      .send({ msg: generateError("Error fetching Sponsors"), error: e });
+  }
+};
+
 const addNewSponsor = async (req: Request, res: Response) => {
   const newBody = _.pick(req.body, [
     "firstName",
@@ -129,7 +149,8 @@ const SponsorMngmntController = {
   getSponsorById,
   addNewSponsor,
   getSponsors,
-  getBusinessCardAvailability
+  getBusinessCardAvailability,
+  getBusinessSponsors
 };
 
 export default SponsorMngmntController;
